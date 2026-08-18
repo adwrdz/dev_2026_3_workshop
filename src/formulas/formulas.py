7fe74db113f5694864e923a4daf7cc05386e04f0
ANDRES FELIPE RODRIGUEZ PRIETO <afrodriguez61@ucatolica.edu.co>
2026-08-18 15:39:45 -0500

    Completado_Raices_Cuadradas

--- src/formulas/formulas.py
+++ src/formulas/formulas.py
@@ -211,6 +211,7 @@ class Formulas:
         return b**2 - 4*a*c
     
     def raices_cuadraticas(self, a, b, c):
+        import math
         """
         Calcula las raíces reales de una ecuación cuadrática ax^2 + bx + c = 0
         usando la fórmula general. Si el discriminante es negativo, lanza ValueError.
@@ -228,7 +229,9 @@ class Formulas:
         Ejemplo:
             raices_cuadraticas(1, -3, 2) -> (2.0, 1.0)
         """
-        pass
+        raiz1 = (-b + math.sqrt(b**2 - 4*a*c))/(2*a)
+        raiz2 = (-b - math.sqrt(b**2 - 4*a*c))/(2*a)
+        return (raiz1, raiz2)
 
     def imc(self, peso, altura):
         """
