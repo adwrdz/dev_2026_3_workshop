c154ab4c99c2d875b2d3d2d189e16e08937a819f
ANDRES FELIPE RODRIGUEZ PRIETO <afrodriguez61@ucatolica.edu.co>
2026-08-18 23:00:55 -0500

    Completado_Es_Triangulo_Valido

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -91,6 +91,7 @@ class Geometria:
             float: Perímetro del triángulo
         """
         
+        
         return lado1 + lado2 + lado3
     
     def es_triangulo_valido(self, lado1, lado2, lado3):
@@ -107,7 +108,13 @@ class Geometria:
         Returns:
             bool: True si los lados pueden formar un triángulo, False en caso contrario
         """
-        pass
+        if (lado1 <= 0 or lado2 <= 0  or lado3 <= 0):
+            return False
+        if (lado1 + lado2 > lado3)and \
+                (lado1 + lado3 > lado2)and \
+                (lado2 + lado3 > lado1) :
+                return True
+        return False 
     
     def area_trapecio(self, base_mayor, base_menor, altura):
         """
